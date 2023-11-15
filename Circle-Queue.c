66a78937cf9e3bdcc0fe8522e08b7065b74b34e4
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2023-11-15 21:46:45 +0530

    Update Circle-Queue.c

--- Circle-Queue.c
+++ Circle-Queue.c
@@ -1,69 +1,85 @@
-#include<stdio.h>
+/*Circle Queue */
 
+#include<stdio.h>
 int F = -1 , R = -1 ;
 int a[10] ;
 
 void main(){
-int item , op = 0 , MAXS;
-printf("Enter the size of Queue : ");
-scanf("%d",&MAXS);
-while(op != 4){
-printf("\nM   E   N   U \n1) Enqueue \n2) Dequeue \n3) Display the queue \n4) Exit the program\n");
-printf("Enter the your choice : ");
-scanf("%d",&op);
-switch(op){
-case 1:
-if(R == MAXS-1){
-printf("The Queue is full \n");
-}
-else{
-printf("Enter the element : ");
-scanf("%d",&item);
-if((F == -1)&&(R == -1)){
-R = R +1;
-a[R] = item;
-F = F + 1;
-}
-else{
-R = R + 1 ;
-a[R] = item;
-}
-}
-break;
+  int item , op = 0 , MAXS;
+  printf("Enter the size of Queue : ");
+  scanf("%d",&MAXS);
+  while(op != 4){
+    printf("\nMenu Of Circle Queue\n--------------------- \n1) Enqueue \n2) Dequeue \n3) Display the queue \n4) Exit the program\n");
+    printf("Enter the your choice : ");
+    scanf("%d",&op);
+    switch(op){
 
-case 2:
-if((F == -1)&&(R == -1)){
-printf("The Queue is Empty \n");
+    /* Enqueue */
+      case 1:
+        if(F == ((R+1) % MAXS)){
+          printf("The Queue is full \n");
+        }
+        else if((F == -1)&&(R == -1)){
+          R = 0;
+          printf("Enter the element : ");
+          scanf("%d",&item);
+          a[R] = item;
+          F = 0;
+        }
+        else{
+          R = (R + 1) % MAXS ;
+          printf("Enter the element : ");
+          scanf("%d",&item);
+          a[R] = item;
 }
-else{
-if(F == R){
-item = a[F];
-F = -1;
-R = -1;
-}
-else{
-item = a[F];
-F = F + 1 ;
-}
-printf("The deleted element is %d \n",item);
-}
-break;
+          break;
+      
+    /* Dequeue */
+      case 2:
+        if((F == -1)&&(R == -1)){
+          printf("The Queue is Empty \n");
+        }
+        else if(F == R){
+          item = a[F];
+          F = -1;
+          R = -1;
+          printf("The deleted element is %d \n",item);
+        }
+        else{
+          item = a[F];
+          F = (F + 1) % MAXS ;
+          printf("The deleted element is %d \n",item);
+        }
+        break;
 
-case 3 :
-printf("The Queue is : ");
-for(int i = F ; i <= R ; i++){
-printf("%d ",a[i]);
-}
-printf("\n");
-break;
+    /* display queue */
+      case 3 :
+        if((F == -1)&&(R == -1)){
+          printf("The Queue is Empty \n");
+        }
+        else if(F == R){
+          printf("The Queue is : ");
+          printf("%d \n",a[F]);
+        }
+        else{
+          int i = F;
+          printf("The Queue is : ");
+          while(i != R){
+            printf("%d ",a[i]);
+            i = ( i + 1 ) % MAXS;
+          }
+          printf("%d\n",a[i]);
+          i = ( i + 1 ) % MAXS;
+        }
+        break;
 
-default:
-printf(" ! ! Invalid choice ! ! \n");
-break;
+    case 4:
+      break;
 
-}
-}
-}
+    default:
+      printf(" ! ! Invalid choice ! ! \n");
+      break;
 
-/*circle queue will be updated soon */
-
+    }
+  }
+}
