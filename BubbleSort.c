9dafee5a71953c329ec1886fd026286d5588e80c
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2023-11-15 08:02:46 +0530

    Update bubble sort

---
+++ BubbleSort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ int bubbleSort(int a[], int n){
+ int temp;
+ for(int i = 0 ; i < n-1 ; i++ ){
+   for( int j = 0 ; j < n-1-i ; j++ ){
+     if(a[j] > a[j+1]){
+       temp = a[j];
+         a[j] = a[j+1];
+         a[j+1] = temp;
+     }
+   }
+ }
+}
+void main(){
+ int i , a[30], n , j , b[30] ;
+ printf("Enter the no of elements: ");
+ scanf("%d",&n);
+ for(i = 0 ; i < n ; i++){
+   printf("Enter the element: ");
+   scanf("%d",&a[i]);
+ }
+ bubbleSort(a,n);
+ printf("\nThe sorted array\n");
+ for(i = 0 ; i < n ; i++ ){
+   printf("%d ",a[i]);
+ }
+}
