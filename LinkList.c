daa3740dbc9dfb70c552337a73053f2ee0761973
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2023-11-26 19:23:54 +0530

    Update LinkList.c

--- LinkList.c
+++ LinkList.c
@@ -1,158 +1,188 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_SIZE 100
+struct node *temp ;
+struct node *ptr ;
+struct node *ptr2;
+struct node *Head = NULL;
+int item ;
 
-// Stack structure
-struct Stack {
-    int top;
-    int items[MAX_SIZE];
+struct node{
+int data;
+struct node *next;
 };
 
-// Function prototypes
-void push(struct Stack *s, int value);
-int pop(struct Stack *s);
-int isOperator(char c);
-int precedence(char c);
-void infixToPostfix(char infix[], char postfix[]);
-int evaluatePostfix(char postfix[]);
 
-int main() {
-    char infix[MAX_SIZE], postfix[MAX_SIZE];
-   
-    printf("Enter the infix expression : ");
-    gets(infix);
-
-    infixToPostfix(infix, postfix);
-
-    printf("Postfix expression is : %s\n", postfix);
-
-    int result = evaluatePostfix(postfix);
-    printf("The result after evaluation is : %d\n", result);
-
-    return 0;
+void create_node(){
+temp = (struct node*) malloc(sizeof(struct node));
+printf("Enter the data : ");
+scanf("%d",&item);
+temp->data = item ;
+temp->next = NULL;
 }
 
-void push(struct Stack *s, int value) {
-    if (s->top == MAX_SIZE - 1) {
-        printf("Stack overflow\n");
-        exit(1);
-    } else {
-        s->items[++(s->top)] = value;
-    }
+/* Insertion */
+
+void insert_At_Head(){
+if(Head == NULL){
+create_node();
+Head = temp;
+}
+else{
+create_node();
+temp->next = Head;
+Head = temp;
+}
+}
+void insert_At_Tail(){
+if(Head == NULL){
+create_node();
+Head = temp;
+}
+else{
+create_node();
+ptr = Head;
+while(ptr->next != NULL){
+ptr = ptr->next;
+}
+ptr->next = temp;
+}
 }
 
-int pop(struct Stack *s) {
-    if (s->top == -1) {
-        printf("Stack underflow\n");
-        exit(1);
-    } else {
-        return s->items[(s->top)--];
-    }
+void insert_At_Pos(){
+int pos;
+printf("Enter the position : ");
+scanf("%d",&pos);
+if(Head == NULL){
+create_node();
+Head = temp;
+}
+else if(pos == 1 )
+insert_At_Head();
+
+else{
+ptr = Head;
+create_node();
+int i = 1;
+while(i<pos){
+ptr = ptr->next;
+i++;
+}
+temp->next = ptr->next;
+ptr->next = temp;
+}
 }
 
-int isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/');
+/* Delection */
+
+void Del_Head(){
+if(Head == NULL){
+printf("List is Empty ");
+}
+else{
+ptr = Head;
+Head = Head->next;
+printf("The deleted element is %d",ptr->data);
+free(ptr);
+}
 }
 
-int precedence(char c) {
-    if (c == '+' || c == '-') {
-        return 1;
-    } else if (c == '*' || c == '/') {
-        return 2;
-    }
-    return 0;
+void Del_Tail(){
+if(Head == NULL){
+printf("List is Empty ");
+}
+else{
+ptr = Head;
+while((ptr->next)->next != NULL){
+ptr = ptr->next;
+}
+ptr2 = ptr->next;
+ptr->next =NULL;
+printf("The deleted element is %d",ptr2->data);
+free(ptr2);
+}
 }
 
-void infixToPostfix(char infix[], char postfix[]) {
-    struct Stack stack;
-    stack.top = -1;
-
-    int i, j;
-    i = j = 0;
-
-    while (infix[i] != '\0') {
-        char symbol = infix[i];
-
-        if (symbol == ' ' || symbol == '\t') {
-            // Ignore whitespace
-            i++;
-        } else if (isdigit(symbol)) {
-            // If the symbol is a digit, copy it to the postfix expression
-            postfix[j++] = symbol;
-            i++;
-        } else if (isOperator(symbol)) {
-            // If the symbol is an operator, pop operators from the stack and append to the postfix expression
-            while (stack.top != -1 && precedence(stack.items[stack.top]) >= precedence(symbol)) {
-                postfix[j++] = pop(&stack);
-            }
-            push(&stack, symbol);
-            i++;
-        } else if (symbol == '(') {
-            // If the symbol is an open parenthesis, push it onto the stack
-            push(&stack, symbol);
-            i++;
-        } else if (symbol == ')') {
-            // If the symbol is a closing parenthesis, pop operators from the stack and append to the postfix expression
-            while (stack.top != -1 && stack.items[stack.top] != '(') {
-                postfix[j++] = pop(&stack);
-            }
-            if (stack.top == -1) {
-                printf("Invalid expression: Unmatched parenthesis\n");
-                exit(1);
-            }
-            // Pop the open parenthesis from the stack
-            pop(&stack);
-            i++;
-        } else {
-            // Invalid character in the infix expression
-            printf("Invalid character in the infix expression: %c\n", symbol);
-            exit(1);
-        }
-    }
-
-    // Pop any remaining operators from the stack and append to the postfix expression
-    while (stack.top != -1) {
-        if (stack.items[stack.top] == '(') {
-            printf("Invalid expression: Unmatched parenthesis\n");
-            exit(1);
-        }
-        postfix[j++] = pop(&stack);
-    }
-    postfix[j] = '\0';  // Null-terminate the postfix expression
+void Del_Pos(){
+int pos;
+printf("Enter the position : ");
+scanf("%d",&pos);
+if(Head == NULL){
+printf("List is Empty ");
 }
-int evaluatePostfix(char postfix[]) {
-    struct Stack stack;
-    stack.top = -1;
-    int i, operand1, operand2;
-    i = 0;
-    while (postfix[i] != '\0') {
-        char symbol = postfix[i];
-        if (isdigit(symbol)) {
-                  push(&stack, symbol - '0');
-        } else if (isOperator(symbol)) {
-            operand2 = pop(&stack);
-            operand1 = pop(&stack);
-  switch (symbol) {
-                case '+':
-                    push(&stack, operand1 + operand2);
-                    break;
-                case '-':
-                    push(&stack, operand1 - operand2);
-                    break;
-                case '*':
-                    push(&stack, operand1 * operand2);
-                    break;
-                case '/':
-                    if (operand2 == 0) {
-                        printf("Error: Division by zero\n");
-                        exit(1);
-                    }
-                    push(&stack, operand1 / operand2);
-                    break;}   }
+else if(pos == 1){
+Del_Head();
+}
+else{
+ptr = Head;
+int i = 1;
+while(i<pos-1){
+ptr = ptr->next;
+i++;
+}
+temp = ptr->next;
+ptr->next = temp->next;
+printf("The deleted element is %d",temp->data);
+free(temp);
+}
+}
 
-        i++;
-    }
-    return pop(&stack);
+void Display(){
+printf("The List is : ");
+ptr = Head;
+while(ptr->next != NULL){
+printf("%d ",ptr->data);
+ptr = ptr->next;
+}printf("%d ",ptr->data);
+}
+
+void main(){
+int op = 0;
+do{
+printf("\nMenu Of Link-List\n1) Insertion at head \n2) Insertion at tail ");
+printf("\n3) Insertion at specific position \n4) Deletion at head \n5) Deletion at tail \n6) Deletion at specific position \n7) Display the List \n8) Exit the program\n");
+
+printf("Enter the choice : ");
+scanf("%d",&op);
+
+switch(op){
+
+case 1:
+insert_At_Head();
+break;
+
+case 2:
+insert_At_Tail();
+break;
+
+case 3:
+insert_At_Pos();
+break;
+
+case 4:
+Del_Head();
+break;
+
+case 5:
+Del_Tail();
+break;
+
+case 6:
+Del_Pos();
+break;
+
+case 7:
+Display();
+break;
+
+case 8:
+break;
+
+default:
+printf("!! Invalid choice !!\n");
+break;
+
+}
+}
+while(op != 8);
 }
